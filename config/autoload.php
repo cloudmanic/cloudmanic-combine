deb62d5243dbe44628c6873925662024937985d5
Andrew Smiley <jayalfredprufrock@gmail.com>
2013-02-09 13:43:06 -0800

    Autoloaded Url helper
    base_url is used in the config file, so the url helper needs to be
    autoloaded. Additionally, the overloaded MY_Loader needs to load helpers
    before loading config files....

--- config/autoload.php
+++ config/autoload.php
@@ -1,4 +1,5 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+$autoload['helper'] = array('url');
 $autoload['config'] = array('combine');
 $autoload['libraries'] = array('combine');
